c356ab47cfe7d141548663e9101aacff385b3dec
Satoshi-Ii <satoshi0104ii@gmail.com>
2025-04-05 16:37:39 +0900

    update test.py and sample.txt

--- test.py
+++ test.py
@@ -1,2 +1,20 @@
 #!/bin/python
-print("this is test!")
+from __future__ import print_function
+import sys
+import re
+import os.path
+
+if len(sys.argv) > 1:
+    f_in = open(sys.argv[1],'r')
+#    root,ext = os.path.splitext(sys.argv[1])
+else:
+    print ('Usage:%s finename' % sys.argv[0])
+    sys.exit()
+
+for ele in f_in:
+#    ele = ele.rstrip(r'\n')
+#    ele = ele.rstrip('\n')
+#    ele1 = re.split(r'\s+',ele)
+    print(ele,end="")
+
+f_in.close()
